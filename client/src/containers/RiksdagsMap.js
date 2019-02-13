992c53f929e77dd2b7e27d7df74df86a4da79445
Oscar Wiigh <oscar.wiigh@gmail.com>
2019-02-13 13:48:19 +0100

    Added data for member seating and installed a react svg library

--- client/src/containers/RiksdagsMap.js
+++ client/src/containers/RiksdagsMap.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import RiksdagsSeats from "../components/RiksdagsSeats/RiksdagsSeats"
 
 export default class RiksdagsMap extends Component {
     render(){
         return (
             <div className="index__map"> 
-            testing
+                <RiksdagsSeats/>
           </div>
         )
     }
