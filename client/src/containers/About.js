a7228748b30834ab5ff75e0d71ccc165a8f78840
Adrian Berger <adigladi@p106-n2.kthopen.kth.se>
2019-02-14 13:33:21 +0100

    Gauge component added

--- client/src/containers/About.js
+++ client/src/containers/About.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Attendance from '../components/Attendance';
 
 class Hello extends Component {
     render() {
       return (
         <div className="about_container">
           <h1> About page </h1>
+          <Attendance></Attendance>
         </div>
       );
     }
