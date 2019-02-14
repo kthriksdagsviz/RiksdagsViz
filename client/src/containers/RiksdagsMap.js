12c5bb1aeda2dd78f14c47e6b15c79bd6919e6bf
Martin Wedberg <status.martin@gmail.com>
2019-02-14 09:41:15 +0100

    Search bar + dropdown: wip

--- client/src/containers/RiksdagsMap.js
+++ client/src/containers/RiksdagsMap.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import RiksdagsSearch from '../components/RiksdagsSearch/RiksdagsSearch'
 
 export default class RiksdagsMap extends Component {
     render(){
         return (
             <div className="index__map"> 
+            {/* <RiksdagsSearch/> */}
             testing
           </div>
         )
---
+++ client/src/utils.js
@@ -0,0 +1,6 @@
+export default 
+    [
+        {"name": "Annie Lööf", "id": 73, "party": "C"},
+        {"name": "Anton Abele", "id": 22, "party": "M"},
+        {"name": "Anny Leif", "id": 74, "party": "C"}
+    ]
