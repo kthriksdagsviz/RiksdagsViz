8db4c64c5db322884a49d98696585b57904d4856
Jonatan Lindström <jonatan3@kth.se>
2019-02-14 15:28:14 +0100

    Chord diagram implemented with dummy values

--- client/src/containers/PartiVotes.js
+++ client/src/containers/PartiVotes.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import PartiCompare from '../components/PartiCompare'
 
 class Parti extends Component {
     render() {
       return (
         <div className="parti_container">
           <h1>Parti page</h1>
+          <PartiCompare />
         </div>
       );
     }
