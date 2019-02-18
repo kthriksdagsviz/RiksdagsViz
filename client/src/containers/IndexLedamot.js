e2c639036e7d12d1493f55b1e99416b0e9de3ff7
Adrian Berger <adigladi@p84-n2.kthopen.kth.se>
2019-02-18 15:35:11 +0100

    Attendance fetch

--- client/src/containers/IndexLedamot.js
+++ client/src/containers/IndexLedamot.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { bindActionCreators } from "redux";
 import * as actions from '../actions'
 import { requestLedamoterByParams } from '../actions'
+import { requestVoteringarById } from '../actions'
 import { connect } from 'react-redux'
+import { votering_api } from '../services'
 import Media from 'react-bootstrap/Media'
 import '../styles/indexLedamot.scss'
 import _ from 'lodash'
@@ -13,6 +15,31 @@ import LedamotComponent from '../components/LedamotComponent/ledamotcomponent';
 
 class IndexLedamot extends Component{
 
+    constructor(props) {
+        super(props);
+        this.state  = {
+            ledamot: {},
+            attendance: {},
+            isFetching: false,
+            fetched: false,
+            error: false
+        }
+      }
+
+      fetchAttendance = (ledamot) => {
+        this.setState({isFetching: true})
+        votering_api.getLedamotVoteringById(ledamot.intressent_id).then((data) => {
+            let path = data.voteringlista.votering[0];
+            let kvot = path.Frånvarande[0] / (parseInt(path.Avstår[0]) + parseInt(path.Frånvarande[0]) + parseInt(path.Ja[0]) + parseInt(path.Nej[0]));
+            let procent = 100 - kvot*100;
+            let attendance = Math.round(procent * 10) / 10;
+        })
+    }
+
+    componentDidMount() {
+        const {ledamot} = this.props
+        this.fetchAttendance(ledamot);
+      }
 
     renderLedamot = () => {
         if(!_.isEmpty(this.props.ledamot)){
@@ -39,7 +66,8 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => {
 //actions:bindActionCreators(actions, dispatch),
     return {
-        ledamoterByParams: (params) => dispatch(requestLedamoterByParams(params))
+        ledamoterByParams: (params) => dispatch(requestLedamoterByParams(params)),
+        voteringarById: (id) => dispatch(requestVoteringarById(id))
     }
 }
     
