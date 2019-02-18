7d666fab933b7a19b1f4514ff65acc22c00b575c
Adrian Berger <adigladi@p84-n2.kthopen.kth.se>
2019-02-18 14:08:59 +0100

    votering fetch updated

--- client/src/services/votering_services.js
+++ client/src/services/votering_services.js
@@ -1,8 +1,9 @@
 
 const base_url = '/api/v1/'
 
-export const getVoteringById = async (id) =>{
-	let response = await fetch(base_url + 'votering/')
+export const getLedamotVoteringById = async (id) =>{
+	//id == ledamotId
+	let response = await fetch(base_url + 'votering/id?iid=' + id)
 	let data = await response.json();
 	return data
 }
