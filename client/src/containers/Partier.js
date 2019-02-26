6ce7498ce9fae80907e57287cfa45e47467a17f9
Martin Wedberg <status.martin@gmail.com>
2019-02-26 16:02:04 +0100

    all parties page created

--- client/src/containers/Partier.js
+++ client/src/containers/Partier.js
@@ -4,6 +4,7 @@ import  Spinner  from 'react-spinkit'
 import _ from 'lodash'
 import { push } from 'connected-react-router'
 import { Link } from 'react-router-dom'
+import "../styles/partier.scss"
 
 class Partier extends React.Component{
     constructor(props){
@@ -14,16 +15,16 @@ class Partier extends React.Component{
         return(
             <div className="partier_page_container">
                 Riksdagspartier
-                <ul>
-                    <li><Link to='/partier/M'> Moderaterna</Link></li>
-                    <li><Link to='/partier/SD'> Sverigedemokraterna</Link></li>
-                    <li><Link to='/partier/L'> Liberalerna</Link></li>
-                    <li><Link to='/partier/KD'> Kristdemokraterna</Link></li>
-                    <li><Link to='/partier/S'> Socialdemokraterna</Link></li>
-                    <li><Link to='/partier/V'> Vänsterpartiet</Link></li>
-                    <li><Link to='/partier/MP'> Miljöpartiet</Link></li>
-
-                </ul>
+                <div className="grid-container">
+                    <div><Link to='/partier/M'><img src='partyLogos/m.png' alt="PartyLogo"  width="40%"/></Link></div>
+                    <div><Link to='/partier/SD'><img src='partyLogos/sd.png' alt="PartyLogo"  width="40%"/></Link></div>
+                    <div><Link to='/partier/L'><img src='partyLogos/l.png' alt="PartyLogo"  width="40%"/></Link></div>
+                    <div><Link to='/partier/KD'><img src='partyLogos/kd.jpg' alt="PartyLogo"  width="40%"/></Link></div>
+                    <div><Link to='/partier/C'><img src='partyLogos/c.png' alt="PartyLogo"  width="40%"/></Link></div>
+                    <div><Link to='/partier/S'><img src='partyLogos/s.png' alt="PartyLogo"  width="40%"/></Link></div>
+                    <div><Link to='/partier/V'><img src='partyLogos/v.png' alt="PartyLogo"  width="40%"/></Link></div>
+                    <div><Link to='/partier/MP'><img src='partyLogos/mp.png' alt="PartyLogo"  width="40%"/></Link></div>
+                </div>
             </div>
         )
     }
