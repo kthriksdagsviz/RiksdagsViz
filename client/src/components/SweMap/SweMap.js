fa1a4f9297e1adbc2a31ed08bcc7d73d23dec871
Adrian Berger <adigladi@Adrians-MacBook-Pro.local>
2019-02-27 16:49:25 +0100

    Map opacity implemented

--- client/src/components/SweMap/SweMap.js
+++ client/src/components/SweMap/SweMap.js
@@ -3,21 +3,9 @@ import { SvgLoader, SvgProxy } from 'react-svgmt';
 import ledamoter from "../../utils/ledamoter.json"
 import * as d3 from 'd3';
 import _ from 'lodash'
-import './SweMap.scss'
-
+//import './SweMap.scss'
+ 
 export default class SweMap extends Component {
-
-    constructor(props){
-        super(props)
-        this.state={
-            selectedCounty: "",
-            build: false,
-            rebuild: false,
-            selectedName: "",
-            fetchedPerson:{},
-            filteredSelection:[]
-        }
-    }
 
     buildSVG = () => {
         var map = <SvgLoader path="/SweMap.svg" width="100%" height="100%" viewBox="0 0 1000 1000">
@@ -36,31 +24,30 @@ export default class SweMap extends Component {
     }
 
     selectCounty = (id) => {
-        console.log(id[Object.keys(id)[0]]);
-        console.log(Object.keys(id).length);
-        var num = 1 / id.length;
         setTimeout(() => {
             let sweMap = d3.select('.swe_map')
             for (var i = 0; i < Object.keys(id).length; i++) {
-                var opacity = 1/(i+1);
+                var opacity = id[Object.keys(id)[i]]/5;
                 var newID = "#" + Object.keys(id)[i];
-                sweMap.select(newID).style("fill", "rgb(90, 150, 255,"+opacity+")");
-                sweMap.select(newID).selectAll('path').style("fill", "rgb(90, 150, 255,"+opacity+")");
-                sweMap.select(newID).selectAll('polygon').style("fill", "rgb(90, 150, 255,"+opacity+")");
+                sweMap.select(newID).style("fill", this.hex2rgba(this.props.color, opacity));
+                sweMap.select(newID).selectAll('path').style("fill", this.hex2rgba(this.props.color, opacity));
+                sweMap.select(newID).selectAll('polygon').style("fill", this.hex2rgba(this.props.color, opacity));
 
-                sweMap.select(newID).attr("class", "info");
-                sweMap.select(newID).selectAll('path').innerHTML += "<span class='infotext'>Antal ledamöter: "+ id[Object.keys(id)[i]] +"</span>";
-                sweMap.select(newID).selectAll('polygon').innerHTML += "<span class='infotext'>Antal ledamöter: "+ id[Object.keys(id)[i]] +"</span>";
+                //sweMap.select(newID).attr("class", "info");
+                //sweMap.select(newID).text("Antal ledamöter: " + id[Object.keys(id)[i]]);
+                //sweMap.select(newID).selectAll('polygon').text("Antal ledamöter: " + id[Object.keys(id)[i]]);
+                //sweMap.select(newID).innerHTML += "<span class='infotext'>Antal ledamöter: "+ id[Object.keys(id)[i]] +"</span>";
+                //sweMap.select(newID).selectAll('path').innerHTML += "<span class='infotext'>Antal ledamöter: "+ id[Object.keys(id)[i]] +"</span>";
+                //sweMap.select(newID).selectAll('polygon').innerHTML += "<span class='infotext'>Antal ledamöter: "+ id[Object.keys(id)[i]] +"</span>";
             }
             
           }, 300);
     }
 
-    destroyMap = () => {
-        d3.select('.swe_map').remove();
-    }
-
-    
+    hex2rgba = (hex, alpha = 1) => {
+        const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16));
+        return `rgba(${r},${g},${b},${alpha})`;
+      };
 
     render() {
     
@@ -68,7 +55,7 @@ export default class SweMap extends Component {
              <div className="swe_map" width="400px" height="700px">
                 {this.buildSVG()}
                 {this.modifySVG()}
-                {this.selectCounty({"W":5, "AB":2, "H":3, "G":1})}
+                {this.selectCounty(this.props.valkrets)}
             </div>
         )
     }
--- client/src/containers/Parti.js
+++ client/src/containers/Parti.js
@@ -6,6 +6,7 @@ import { push } from 'connected-react-router'
 import SweMap from "../components/SweMap/SweMap.js"
 import { ledamoter_api } from '../services'
 import { requestLedamoterByParty } from '../actions'
+import partyColors from '../styles/colors.scss'
 
 class PartiPage extends React.Component{
     constructor(props){
@@ -16,7 +17,8 @@ class PartiPage extends React.Component{
             isFetching: false,
             fetched: false,
             error: false,
-            ledamoter:{}
+            ledamoter:{},
+            valkrets:{}
         }
     }
 
@@ -27,7 +29,7 @@ class PartiPage extends React.Component{
             var k=0;
             var m=0;
             var temp = [];
-            var valkretsList = [];
+            var valkretsList = {};
             var valkretsar = {"Blekinge län":"K" ,"Dalarnas län":"W",
                               "Gotlands län":"I","Gävleborgs län":"X","Göteborgs kommun":"O",
                               "Hallands län":"N","Jämtlands län":"Z",
@@ -37,14 +39,14 @@ class PartiPage extends React.Component{
                               "Stockholms län":"AB","Stockholms kommun":"AB",
                               "Södermanlands län":"D","Uppsala län":"C",
                               "Värmlands län":"S","Västerbottens län":"AC",
-                              "Västernorrlands län":"Y","Västermanlands län":"U",
+                              "Västernorrlands län":"Y","Västmanlands län":"U",
                               "Västra Götalands läns norra":"O","Västra Götalands läns västra":"O","Västra Götalands läns östra":"O",
                               "Västra Götalands läns södra":"O","Örebro län":"T",
                               "Östergötlands län":"E"}
                             
             if(data['@hits'] > 0){
                 temp = [];
-                valkretsList = [];
+                valkretsList = {};
                 for (var i = 0; i < data.person.length; i++) {
                     if (data.person[i].kon == 'kvinna'){
                         k += 1;
@@ -53,14 +55,17 @@ class PartiPage extends React.Component{
                         m += 1;
                     }
                     if (data.person[i].valkrets) {
-                        temp.push(data.person[i].valkrets);
+                        var key = valkretsar[data.person[i].valkrets];
+                        //valkretsList.push(valkretsar[data.person[i].valkrets]);
+                        if (!(key in valkretsList)) {
+                            valkretsList[key] = 1;
+                        }
+                        else {
+                            valkretsList[key] += 1;
+                        }
                     }
                 }
-                this.setState({man: m, kvinnor: k})
-                for (var j = 0; j < temp.length; j++) {
-                    valkretsList.push(valkretsar[temp[j]]);
-                }
-                console.log(valkretsList.sort());
+                this.setState({man: m, kvinnor: k, valkrets: valkretsList})
                 this.setState({ledamoter: data.person, fetched: true, isFetching: false})
             }
             else{
@@ -86,7 +91,7 @@ class PartiPage extends React.Component{
             <div className="parti_page_container">
                 <img src={process.env.PUBLIC_URL +  '/parties_loggor/' + match.params.parti + '.png'} alt="PartyLogo"  width="10%"/>
                 {match.params.parti}
-                <SweMap/>
+                <SweMap valkrets={this.state.valkrets} color={partyColors["party" + match.params.parti]}/>
             </div>
         )
     }
