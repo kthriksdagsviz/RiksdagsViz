6ab7326d057bdc8c6becc630db56da63926bdbf6
Martin Wedberg <status.martin@gmail.com>
2019-02-27 12:53:13 +0100

    wip

--- client/src/containers/PartiInfo.js
+++ client/src/containers/PartiInfo.js
@@ -4,30 +4,45 @@ import  Spinner  from 'react-spinkit'
 import _ from 'lodash'
 import { push } from 'connected-react-router'
 import { Link } from 'react-router-dom'
+import SweMap from "../components/SweMap/SweMap.js"
+import { ledamoter_api } from '../services'
+import { requestLedamoterByParty } from '../actions/index.js';
 
 class PartiInfo extends React.Component{
     constructor(props){
         super(props);
     }
 
+    fetchLedamoter = () => {
+        this.setState({isFetching: true})
+        ledamoter_api.getLedamoterByParty().then((data) => {
+            console.log(data);
+        })
+    }
+
+    componentDidMount(){
+        this.fetchLedamoter()
+    }
+
     render(){   
         return(
             <div className="partier_page_container">
                 Partier page 
-                <ul>
-                    <li><Link to='/partier/M'> Moderaterna</Link></li>
-                    <li><Link to='/partier/SD'> Sverigedemokraterna</Link></li>
-                    <li><Link to='/partier/L'> Liberalerna</Link></li>
-                    <li><Link to='/partier/KD'> Kristdemokraterna</Link></li>
-                    <li><Link to='/partier/S'> Socialdemokraterna</Link></li>
-                    <li><Link to='/partier/V'> Vänsterpartiet</Link></li>
-
-                </ul>
+                <SweMap></SweMap>
             </div>
         )
     }
 }
+const mapStateToProps = state => ({
+    ledamoter: state.ledamoter
+})
 
+const mapDispatchToProps = dispatch => {
+//actions:bindActionCreators(actions, dispatch),
+    return {
+        ledamoterByParty: () => dispatch(requestLedamoterByParty()),
+    }
+}
     
-  export default connect(null, null)(PartiInfo);
+  export default connect(mapStateToProps, mapDispatchToProps)(PartiInfo);
     
