1e1e1efc7a759703669e6cfb2930cb5c2e403961
Emil Dickson <emil.dickson@gmail.com>
2019-02-27 11:50:35 +0100

    Fixing a new headline for voting years as well as other fixes for the timeline.

--- client/src/components/PartiChord.js
+++ client/src/components/PartiChord.js
@@ -179,7 +179,10 @@ export default class PartiChord extends React.Component{
               break 
             case 2017:
               yearString = '1718'
-              break 
+              break
+            case 2018:
+              yearString = 'now'
+              break
             default:
               yearString = 'now'
           }
