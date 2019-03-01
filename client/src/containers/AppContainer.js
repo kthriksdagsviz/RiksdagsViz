75a6350184f515532a9be65ee23cf82745ede602
Emil Dickson <emil.dickson@gmail.com>
2019-03-01 10:43:20 +0100

    "Tutorial" information added to the first page.

--- client/src/containers/AppContainer.js
+++ client/src/containers/AppContainer.js
@@ -20,12 +20,28 @@ const steps = [
     target: '.riksdags_map',
     disableBeacon: 'true',
     placement:'bottom',
-    content: 'Welcome to my visualization! Follow these steps to learn more about the World Value Surveys and Gapminder. Press the red circles to learn how to use this visualization!',
+    content: 'Här ser du en visualisering av alla stolarna i Sveriges riksdag! För musmarkören över en stol för att se vem som sitter på den platsen och klicka för mer information.',
   },
   {
     target: '.search-container',
-    content: 'Welcome to my visualization! Follow these steps to learn more about the World Value Surveys and Gapminder. Press the red circles to learn how to use this visualization!',
+    content: 'Sök på specifika ledamöter! Klicka på en person för att se mer information. Du kan även filtrera på parti och se var dess ledamöter sitter.',
   },
+  {
+    target: '.navbar_link_ledamoter',
+    content: 'Här finns en sökfunktion för individuella ledamöter.'
+  },
+  {
+    target: '.navbar_link_partier',
+    content: 'Här finns ett diagram med information om hur partierna har röstat i olika frågor, i förhållande till varandra.'
+  },
+  {
+    target: '.navbar_link_parties',
+    content: 'Här hittar du mer information om varje parti.'
+  },
+  {
+    target: '.navbar_link_about',
+    content: 'Här kan du få veta mer om teamet som ligger bakom RiksdagsViz!'
+  }
   
 ]
 
