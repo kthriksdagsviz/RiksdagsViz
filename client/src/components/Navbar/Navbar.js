260289f6097f271d25850e3e4c11afccd0d33a4f
Ben Forsrup <ben@newstag.com>
2019-03-01 11:02:48 +0100

    changed navbuttons

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUsers, faLandmark, faLayerGroup, faRunning, faHandshake } from '@fortawesome/free-solid-svg-icons'
+import { faUsers, faInfoCircle, faChartPie, faLandmark, faGripHorizontal, faRunning, faHandshake } from '@fortawesome/free-solid-svg-icons'
 import './navbar.scss'
 
 export default () => (
@@ -13,22 +13,22 @@ export default () => (
         </Link>
         <Link to="/ledamoter">
             <div className="navbar_link_ledamoter" title="Ledamöter">
-            <FontAwesomeIcon icon={faRunning} size="3x" color="white" />      
+            <FontAwesomeIcon icon={faUsers} size="3x" color="white" />      
             </div> 
         </Link>
         <Link to="/partier">
             <div className="navbar_link_partier" title="Voteringslikhet mellan partier">
-                <FontAwesomeIcon icon={faLayerGroup} size="3x" color="white" />   
+                <FontAwesomeIcon icon={faChartPie} size="3x" color="white" />   
             </div> 
         </Link>
         <Link to="/parties">
             <div className="navbar_link_parties" title="Partier">
-                <FontAwesomeIcon icon={faHandshake} size="3x" color="white" />   
+                <FontAwesomeIcon icon={faGripHorizontal} size="3x" color="white" />   
             </div> 
         </Link>
         <Link to="/about">
             <div className="navbar_link_about" title="Om RiksdagsViz">
-                <FontAwesomeIcon icon={faUsers} size="3x" />
+                <FontAwesomeIcon icon={faInfoCircle} size="3x" />
             </div> 
         </Link>
     </div>
